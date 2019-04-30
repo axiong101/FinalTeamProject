85cc03772f3960596e17ce9f7100bc0fc94cc25d
Alvin Xiong <32112712+axiong101@users.noreply.github.com>
2019-04-29 21:00:09 -0500

    Add files via upload

---
+++ QuestionNode.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.ArrayList;
+
+public class QuestionNode {
+  private String question;
+  private boolean TorF;
+  private ArrayList<AnswerNode> answerList;
+
+
+  public QuestionNode(String question, AnswerNode answerA, AnswerNode answerB, AnswerNode answerC,
+      AnswerNode answerD, Boolean TorF) {
+    this.question = question;
+    answerList = new ArrayList<AnswerNode>();
+    answerList.add(answerA);
+    answerList.add(answerB);
+    answerList.add(answerC);
+    answerList.add(answerD);
+    this.TorF = TorF;
+  }
+  
+  public String getQuestion() {
+    return this.question;
+  }
+  
+  public boolean TorF() {
+    return this.TorF;
+  }
+    
+  public ArrayList<AnswerNode> getAnswerList() {
+    return this.answerList;
+  }
+    
+}
