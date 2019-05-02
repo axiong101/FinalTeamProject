3e58f5fd0252bdcd6c7621d0a65eb5d098dde944
Alvin Xiong <32112712+axiong101@users.noreply.github.com>
2019-05-01 19:58:01 -0500

    Add files via upload

--- AnswerButton.java
+++ AnswerButton.java
@@ -5,9 +5,9 @@ import javafx.scene.control.Button;
 public class AnswerButton extends Button{
   boolean correct;
 
-  protected AnswerButton(String answer, boolean correct) {
-    this.setText(answer);
-    this.correct = correct;
+  protected AnswerButton(AnswerNode answer) {
+    this.setText(answer.getAnswer());
+    this.correct = answer.getCorrect();
       
   }
   public boolean getBoolean() {
